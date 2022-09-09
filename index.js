3683344c1cdde22be0b18e361a0ad13c36670687
Sajini Ariyawansa <dhanushni@gmail.com>
2022-09-09 16:17:57 +1000

    fix: code issues detected from sonarcloud

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ module.exports = function(handler) {
   if (!(handler instanceof Function || handler instanceof RegExp))
     throw new Error('Unsupported param handler. Either function or Regexp expected')
 
-  return function(req, res, next, val, name) {
-    val = req.params[name]
+  return function(req, _res, next, _val, name) {
+    const handlerValue = req.params[name];
 
     if (handler instanceof Function) {
-      var newVal = handler(val)
+      const newVal = handler(handlerValue)
       if (!newVal)
         return next('route'); // bypass current route
       req.params[name] = newVal
 
     } else if (handler instanceof RegExp) {
-      if (!handler.test(val))
+      if (!handler.test(handlerValue))
         return next('route') // bypass current route
 
     } else {
