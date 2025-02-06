5b5dbb760c51d63e33e428b96681eb7f8e362c7b
Benny Chew <benny.chew@adslot.com>
2025-02-06 12:24:59 +1100

    ci: add linting

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-
-module.exports = function(handler) {
+module.exports = function (handler) {
   if (!(handler instanceof Function || handler instanceof RegExp))
-    throw new Error('Unsupported param handler. Either function or Regexp expected')
+    throw new Error('Unsupported param handler. Either function or Regexp expected');
 
-  return function(req, _res, next, _val, name) {
+  return function (req, _res, next, _val, name) {
     const handlerValue = req.params[name];
 
     if (handler instanceof Function) {
-      const newVal = handler(handlerValue)
-      if (!newVal)
-        return next('route'); // bypass current route
-      req.params[name] = newVal
-
+      const newVal = handler(handlerValue);
+      if (!newVal) return next('route'); // bypass current route
+      req.params[name] = newVal;
     } else if (handler instanceof RegExp) {
-      if (!handler.test(handlerValue))
-        return next('route') // bypass current route
-
+      if (!handler.test(handlerValue)) return next('route'); // bypass current route
     } else {
-      throw new Error('Should not reach here')
+      throw new Error('Should not reach here');
     }
 
     return next();
-  }
-}
+  };
+};
--- test/index.js
+++ test/index.js
@@ -1,83 +1,71 @@
-var assert = require('assert')
-var request = require('supertest')
-var express = require('express')
-var lib = require('../')
+var assert = require('assert');
+var request = require('supertest');
+var express = require('express');
+var lib = require('../');
 
+describe('express-params-handler', function () {
+  var makeApp = function () {
+    var app = express();
+    app.param('id', lib(Number));
+    app.param('date', lib(/^\d{4}-\d{2}-\d{2}$/));
 
-describe('express-params-handler', function() {
+    var handler = function (req, res, _next) {
+      res.json({ params: req.params });
+    };
 
-  var makeApp = function() {
-    var app = express()
-    app.param('id', lib(Number))
-    app.param('date', lib(/^\d{4}-\d{2}-\d{2}$/))
+    app.get('/by-id/:id', handler);
+    app.get('/by-date/:date', handler);
+    return app;
+  };
 
-    var handler = function(req, res, next) {
-      res.json({params: req.params})
-    }
-
-    app.get('/by-id/:id', handler)
-    app.get('/by-date/:date', handler)
-    return app
-  }
-
-
-  describe('function handlers', function() {
-
-    it('positive', function(done) {
+  describe('function handlers', function () {
+    it('positive', function (done) {
       request(makeApp())
         .get('/by-id/100')
-        .expect(200, function(err, res) {
-          if (err) return done(err)
-          assert.deepEqual(res.body.params, {id: 100})
-          done()
-        })
-    })
+        .expect(200, function (err, res) {
+          if (err) return done(err);
+          assert.deepEqual(res.body.params, { id: 100 });
+          done();
+        });
+    });
 
-    it('negative', function(done) {
+    it('negative', function (done) {
       request(makeApp())
         .get('/by-id/kraken')
-        .expect(404, function(err, res) {
-          if (err) return done(err)
-          assert.deepEqual(res.body, {})
-          done()
-        })
-    })
+        .expect(404, function (err, res) {
+          if (err) return done(err);
+          assert.deepEqual(res.body, {});
+          done();
+        });
+    });
+  });
 
-  })
-
-
-  describe('regexp handlers', function() {
-
-    it('positive', function(done) {
+  describe('regexp handlers', function () {
+    it('positive', function (done) {
       request(makeApp())
         .get('/by-date/2015-07-30')
-        .expect(200, function(err, res) {
-          assert.deepEqual(res.body.params, {date: '2015-07-30'})
-          done()
-        })
-    })
+        .expect(200, function (_err, res) {
+          assert.deepEqual(res.body.params, { date: '2015-07-30' });
+          done();
+        });
+    });
 
-    it('negative', function(done) {
+    it('negative', function (done) {
       request(makeApp())
         .get('/by-date/kraken')
-        .expect(404, function(err, res) {
-          if (err) return done(err)
-          assert.deepEqual(res.body, {})
-          done()
-        })
-    })
+        .expect(404, function (err, res) {
+          if (err) return done(err);
+          assert.deepEqual(res.body, {});
+          done();
+        });
+    });
+  });
 
-  })
-
-
-  describe('unsupported handler type', function() {
-
-    it('should complain about invalid handler type', function() {
-      assert.throws(function() {
-        lib('nonsense')
-      }, /Unsupported param handler/)
-    })
-
-  })
-
-})
+  describe('unsupported handler type', function () {
+    it('should complain about invalid handler type', function () {
+      assert.throws(function () {
+        lib('nonsense');
+      }, /Unsupported param handler/);
+    });
+  });
+});
